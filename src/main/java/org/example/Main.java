07ac7819b4457cba4086460a00ebd1ed7a3dee86
紫红 <zihong@micous.com>
2025-03-31 16:10:40 +0800

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author: zihong@micous.com
+ * @date: 2025/3/31 15:34
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
